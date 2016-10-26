82f1f7eb22c429e09e0fc6762921be2061ca91f1
Xiao Xiao <xiao3259110@gmail.com>
2016-10-26 10:32:36 -0400

    Create MaxAlternationSliceSize.java

---
+++ MaxAlternationSliceSize.java
@@ -0,0 +1,18 @@
+/**Given array with positive and negative values, return the maximum alternating slice size, two elements are alternating if they have different signs, zero is treating as both negative and positive.
+
+Example
+Given a = [1, -5, 23, 0, 1, 1, 0, 2, -5, 3, -1, 1, 2, 3] return 7 (the sequence [1, 0, 2, -5, 3, -1, 1] has maximum alternating slice size )
+
+The expected runtime is o(n).*//
+
+
+public class Solution{
+
+    public int maxAlternationSliceSize(int[] a){
+        /**
+    
+    
+    }
+
+
+}
